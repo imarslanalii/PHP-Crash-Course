ba0c37ffd3e5cd492036834c789cb9ab57a846db
imarslanalii <imarslanali9@gmail.com>
2024-09-13 16:52:31 +0500

    Initial commit

---
+++ variables.php
@@ -0,0 +1,27 @@
+<?php
+// prefix -$ 
+
+// echo 'Helo this is Arslan Ali'
+
+// $output = 'Hello World!';
+// echo $output;
+
+$num1 = 29;
+$num2 = 1;
+$sum = $num1 + $num2;
+
+// echo $sum;
+
+// $str3 = 'They\'re Here';
+$str3 = "They\'re Here";
+
+$str1 = 'Hey';
+$str2 = 'Ali';
+$greeting = $str1 .' '. $str2;
+// $greeting2 = '$str1 $str2';
+$greeting2 = "$str1 $str2";
+
+// We use define function when we know next str can't changed.
+define ('GREETING', 'Hello Everyone'); 
+echo GREETING;
+?>
