03850a3e8e3a79f256e338c5453e55edd680b85d
imarslanalii <imarslanali9@gmail.com>
2024-09-15 15:56:28 +0500

    Create loops.php

---
+++ loops.php
@@ -0,0 +1,49 @@
+<?php
+// For lop
+// Params - init, condition, inc
+
+// for ($i = 0;$i < 10; $i++){
+//     // echo $i;
+//     echo 'Number'. $i;
+//     echo '<br>';
+// }
+
+
+
+// While loop
+// Params - condition
+
+// $i = 0;
+
+// while ($i < 10){
+//     echo $i;
+//     echo '<br>';
+//     $i++;
+
+// }
+
+
+// do.... While lo0p
+// Params - condition
+
+// $i = 0;
+
+// do{
+//     echo $i;
+//     echo '<br>';
+//     $i++;
+// }
+
+// while ($i < 10);
+
+// For Each.... loop
+
+
+
+$people = array('Kevin', 'Jeny', 'Sara');
+
+foreach($people as $person){
+    echo $person;
+    echo '<br>';
+}
+?>
