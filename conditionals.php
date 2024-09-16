17e414ab2538b950f26432f5e352f01cb7c4701d
imarslanalii <imarslanali9@gmail.com>
2024-09-16 11:44:13 +0500

    Update conditionals.php

---
+++ conditionals.php
@@ -0,0 +1,50 @@
+<?php
+// Conditionals 
+
+
+// $num = 4;
+
+// if($num >= 5){
+//     echo '5 passed';
+// } else if ($num == 4){
+//     echo '4 passed';
+// } else {
+//     echo 'did not passed';
+// }
+
+// Nested if 
+
+// $num = 6;
+
+
+// if ($num > 4){
+//     if ($num < 10){
+//         echo "$num Passed";
+//     } else {
+//         echo 'Ehatever';
+//     }
+// }
+
+// Logical Operator (and &&, or ||, xor)
+
+
+// if ($num > 4 AND $num < 10){
+//     echo "$num passed";
+// }
+
+
+// Switch 
+
+$favClolor = 'yellow';
+
+switch($favClolor){
+    case 'red':
+        echo 'Your Favourite Color is Red';
+        break;
+    case 'blue':
+        echo 'Your Favourite Color is blue';
+        break;
+    default:
+        echo 'Your Favourite Color is something else';
+}
+?>
