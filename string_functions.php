fe4fa1b478a66d069a201301db20cd9757c2b857
imarslanalii <imarslanali9@gmail.com>
2024-09-18 15:05:41 +0500

    Update string_functions.php

---
+++ string_functions.php
@@ -0,0 +1,45 @@
+<?php
+# substr()
+
+
+// $output = substr('Hello', 1, 3);
+// $output = substr('Hello', -2);
+
+// echo $output;
+
+# strlen()
+
+// $output = strlen('Hello World!');
+
+// echo $output;
+
+# strpos()
+
+// $output = strpos('Hello World!', 'o');
+
+// echo $output;
+
+# strrpos()
+
+// $output = strrpos('Hello World!');
+
+// echo $output;
+
+# strtoupper
+
+// $output = strtoupper('Hello World!');
+
+// echo $output;
+
+# gzcompress
+$string = "Hello this is Ali";
+
+$compressed = gzcompress($string);
+
+// echo $compressed;
+
+$original = gzuncompress($compressed);
+
+echo $original;
+
+?>
