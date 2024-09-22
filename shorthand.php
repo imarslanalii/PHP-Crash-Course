17082ba594d0b975f9bd2b99529fcf6e46b72f8c
imarslanalii <imarslanali9@gmail.com>
2024-09-22 15:23:15 +0500

    Create shorthand.php

---
+++ shorthand.php
@@ -0,0 +1,29 @@
+<?php
+
+$loggedIn = false;
+
+// if ($loggedIn){
+//     echo 'You are a logged in';
+//     } else { 
+//         echo 'You are not logged in';
+//     }
+
+// echo ($loggedIn) ? 'You are a logged in' : 'You are not logged in';
+
+// $isRegistred = ($loggedIn == true) ? true : false; 
+
+// echo $isRegistred;
+
+$age = 7;
+$score = 15;
+
+echo 'Your score is: '.($score > 10 ? ($age > 10 ? 'Average':
+'Exception'): ($age > 10 ? 'Horrib;e': 'Average'));
+
+?>
+
+
+
+
+
+
