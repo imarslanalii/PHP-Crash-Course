27c3b8e5e70a185d2948789955d88cfc6eb1ea16
imarslanalii <122626416+imarslanalii@users.noreply.github.com>
2025-02-05 12:19:04 +0500

    Update date.php

--- date.php
+++ date.php
@@ -13,7 +13,6 @@ date_default_timezone_set('America/New_York');
 // echo date('h: i: sa');
 $timestamp = mktime(14, 14, 54, 06, 21, 2002);
 // echo date('m/d/y h:i:sa', $timestamp);
-
 $timestamp2 = strtotime('7:00pm March 22 2024');
 $timestamp2 = strtotime('tomorrow');
 $timestamp2 = strtotime('next Sunday');
