74652576b9779fb762d19df7c84690d08aff6ba2
imarslanalii <122626416+imarslanalii@users.noreply.github.com>
2025-02-22 18:09:14 +0500

    Update cookies.php

--- cookies.php
+++ cookies.php
@@ -6,7 +6,6 @@
 // Since cookies are stored on the client, you shouldn't store sensitive data in them. Sessions are a better choice for storing sensitive data.
 // Set a cookie
 setcookie('name', 'Arslan', time() + 86400 * 30); // 86400 = 1 day
-
 // echo time();
 
 // Get a cookie
