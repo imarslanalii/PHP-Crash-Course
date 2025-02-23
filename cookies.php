0e607f07006258e4230a804160ef1097587cc289
imarslanalii <122626416+imarslanalii@users.noreply.github.com>
2025-02-23 11:19:55 +0500

    Update cookies.php

--- cookies.php
+++ cookies.php
@@ -12,7 +12,6 @@ setcookie('name', 'Arslan', time() + 86400 * 30); // 86400 = 1 day
 if (isset($_COOKIE['name'])) {
   echo $_COOKIE['name'];
 }
-
 // Delete a cookie
 setcookie('name', '', time() - 86400);
 
