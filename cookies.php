78a1fd7c3b45e6a22e96f9ca607073a15ed820c0
imarslanalii <122626416+imarslanalii@users.noreply.github.com>
2025-02-25 08:02:20 +0500

    Update cookies.php

--- cookies.php
+++ cookies.php
@@ -7,7 +7,6 @@ Cookies are a mechanism for storing data in the remote browser and thus tracking
 // Set a cookie
 setcookie('name', 'Arslan', time() + 86400 * 30); // 86400 = 1 day
 // echo time();
-
 // Get a cookie
 if (isset($_COOKIE['name'])) {
   echo $_COOKIE['name'];
