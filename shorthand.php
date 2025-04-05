4cf35589ae2cba4102d4551e63417f28ec14345e
imarslanalii <122626416+imarslanalii@users.noreply.github.com>
2025-04-05 16:59:35 +0500

    Update shorthand.php

--- shorthand.php
+++ shorthand.php
@@ -8,7 +8,6 @@ $loggedIn = false;
 // echo ($loggedIn) ? 'You are a logged in' : 'You are not logged in';
 // $isRegistred = ($loggedIn == true) ? true : false; 
 // echo $isRegistred;
-
 $age = 7;
 $score = 15;
 echo 'Your score is: '.($score > 10 ? ($age > 10 ? 'Average':
